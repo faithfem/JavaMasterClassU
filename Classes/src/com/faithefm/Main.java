14b5cf289ce5e8edf5b7ca9cea6d4d07852024fe
faithfem <fmugure@intracitygeeks.org>
2018-02-05 23:48:30 -0500

    Classes ThirtySix

--- Classes/src/com/faithefm/Main.java
+++ Classes/src/com/faithefm/Main.java
@@ -3,6 +3,14 @@ package com.faithefm;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Car porsche = new Car(); //created a new object, porsche based on template car
+        Car toyota = new Car();
+
+        porsche.setModel("Carrera");
+
+        System.out.println("Model is " + porsche.getModel());
+
+        //When you create CLASSES, make sure you always use the KEYWORD "NEW"
+
     }
 }
