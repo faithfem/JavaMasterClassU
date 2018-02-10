8c8af8fa89c34a67c9736ffa174abfad35bb40ce
faithfem <fmugure@intracitygeeks.org>
2018-02-10 10:26:07 -0500

    Inheritance Sec7

--- Inheritance/src/com/faithefm/Dog.java
+++ Inheritance/src/com/faithefm/Dog.java
@@ -16,7 +16,7 @@ public class Dog extends Animal{
     // CREATE A CONSTRUCTOR THAT CALLS THE CONSTRUCTOR FOR ANIMAL
 
     public Dog(/*int brain, int body,*/String name, int size, int weight, int eyes, int legs, int tail, int teeth, String coat) { //INITIALIZE DOGS PARAMS TOO
-        super(1,1,5,4,5,); //SUPER CLASS IS THE ANIMAL CLASS.
+        super("Poodle",1,5,4,5); //SUPER CLASS IS THE ANIMAL CLASS.
         // INITIALIZED BRAIN & BODY INSIDE THE CONSTRUCTOR, SO NOT NEEDED IN LINE ABOVE THAT SO COMMENTED OUT
         this.eyes = eyes;
         this.legs = legs;
--- Inheritance/src/com/faithefm/Main.java
+++ Inheritance/src/com/faithefm/Main.java
@@ -3,9 +3,9 @@ package com.faithefm;
 public class Main {
 
     public static void main(String[] args) {
-        Animal animal = new Animal(1, 1,5,5,10);
+        Animal animal = new Animal("Animal",1,1,5,5);
 
-        Dog dog = new Dog(2,5,"Yorkie",2,4,1,20,"Smooth");
+        Dog dog = new Dog("Yorkie",8,20,2,4,1,20,"Long Silky");
         dog.eat();
         dog.walk();
         dog.run();
